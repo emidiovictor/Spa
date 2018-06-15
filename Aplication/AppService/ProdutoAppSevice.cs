cba0148897e4372be4b69d181f8267365c10b890
Victor Emidio <victoremidio@yahoo.com.br>
2018-06-15 00:27:37 -0300

    Creating the last app services

---
+++ Aplication/AppService/ProdutoAppSevice.cs
@@ -0,0 +1,21 @@
+﻿using Aplication.Interfaces;
+using Domain.Entity;
+using Domain.Interfaces;
+using InfraData.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UoW;
+
+namespace Aplication.AppService
+{
+    public class ProdutoAppSevice : AppServiceBase<Produto, ProdutoViewModel>,IProdutoAppService
+    {
+        public ProdutoAppSevice(IProdutoRepository repository, IUnitOfWork unitOfWork, Context context):base(repository,unitOfWork,context)
+        {
+                
+        }
+    }
+}
---
+++ Aplication/AppService/ServicoAppService.cs
@@ -0,0 +1,22 @@
+﻿using Aplication.Interfaces;
+using Domain.Entity;
+using InfraData.Repositories;
+using InfraData.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UoW;
+
+namespace Aplication.AppService
+{
+    public class ServicoAppService : AppServiceBase<Servico,ServicoViewModel>, IServicoAppService
+    {
+        public ServicoAppService(ServicoRepository repository,IUnitOfWork unitOfWork, Context context):
+            base(repository,unitOfWork,context)
+        {
+
+        }
+    }
+}
